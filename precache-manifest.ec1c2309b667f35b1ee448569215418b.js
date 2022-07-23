cb7bc98eaae015380b4c364282edff4471708a5b
Akash More <akashkmore1@gmail.com>
2022-07-23 20:13:54 +0530

    Updates

---
+++ precache-manifest.ec1c2309b667f35b1ee448569215418b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b3cc03d6d699460f98",
+    "url": "/Portfolio-AkashM/static/css/main.e61ee687.chunk.css"
+  },
+  {
+    "revision": "b8b3cc03d6d699460f98",
+    "url": "/Portfolio-AkashM/static/js/main.b8b3cc03.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/Portfolio-AkashM/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "d6dcdd45ea7562832c16",
+    "url": "/Portfolio-AkashM/static/js/runtime~main.d6dcdd45.js"
+  },
+  {
+    "revision": "a52b8dcbbf4f18fa5e7d896b913bf83c",
+    "url": "/Portfolio-AkashM/index.html"
+  }
+];
