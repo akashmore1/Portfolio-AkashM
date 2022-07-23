6892bb7989569679cf81b247902aab43cc8386ab
Akash More <akashkmore1@gmail.com>
2022-07-23 20:12:36 +0530

    Test branch for production

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1 className="heading-initial__app">Hello World!</h1>;
+  return (
+    <>
+      <h1 className="heading-initial__app">Hello World!</h1>
+      <h2 className="heading-initial__app">Portfolio of Akash More</h2>
+    </>
+  );
 };
 
 export default App;
