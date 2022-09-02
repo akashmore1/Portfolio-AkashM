3721302b70b33f37c35c6dc1780162a8c6434516
Akash More <akashkmore1@gmail.com>
2022-09-02 21:38:32 +0530

    Create portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Portfolio.css";
+import Img from "../../assets/maxresdefault.jpeg";
+import Snake from "../../assets/snake.jpeg";
 
 function Portfolio() {
   return (
@@ -7,12 +9,44 @@ function Portfolio() {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article>
+        <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <h3>This is portfolio item title</h3>
-            <a href="https://www.github.com" target="_blank">
-              Github
-            </a>
+            <div>
+              <img src={Img} alt="img" />
+            </div>
+            <h3>Voice Automated Robot</h3>
+            <div className="portfolio__item-cta">
+              {/* <a href="https://www.github.com" target="_blank" className="btn">
+                Github
+              </a> */}
+              <a
+                href="https://youtube.com/shorts/rIjHnXHsLiI?feature=share"
+                target="_blank"
+                className="btn btn-primary"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <div>
+              <img src={Snake} alt="img" />
+            </div>
+            <h3>Snake Game</h3>
+            <div className="portfolio__item-cta">
+              <a href="https://www.github.com" target="_blank" className="btn">
+                Github
+              </a>
+              <a
+                href="https://www.github.com"
+                target="_blank"
+                className="btn btn-primary"
+              >
+                Live Demo
+              </a>
+            </div>
           </div>
         </article>
       </div>
