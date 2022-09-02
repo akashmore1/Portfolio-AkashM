35d13f6f26c0e9ed5f328b307e2105483ccf21c2
Akash More <akashkmore1@gmail.com>
2022-09-02 20:53:36 +0530

    Start portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Portfolio.css";
 
 function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article>
+          <div className="portfolio__item-image">
+            <h3>This is portfolio item title</h3>
+            <a href="https://www.github.com" target="_blank">
+              Github
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
