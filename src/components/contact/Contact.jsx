f766993155b9961aa70db672afa63916f15f25c5
Akash More <akashkmore1@gmail.com>
2022-09-17 21:54:02 +0530

    Add section for messeging platforms

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLinkedinBoxFill } from "react-icons/ri";
 
 const Contact = () => {
-  return <section id="contact"></section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>dummy@gmail.com</h5>
+            <a href="mailto:akashkmore1@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiLinkedinBoxFill />
+            <h4>Linkedin Messenger</h4>
+            <h5>Akash More</h5>
+            <a href="dummy@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/* End of options */}
+        <form action=""></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
