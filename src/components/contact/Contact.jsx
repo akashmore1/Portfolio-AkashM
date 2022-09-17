03bf722b7fb37ec8900d2a40f91142ac824565a7
Akash More <akashkmore1@gmail.com>
2022-09-17 22:06:11 +0530

    Complete jsx part in contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,7 +29,26 @@ const Contact = () => {
           </article>
         </div>
         {/* End of options */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
