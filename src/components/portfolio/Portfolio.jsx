4fcc16354804a776ed3cef71466ac8bafd3e5b56
Akash More <akashkmore1@gmail.com>
2022-10-10 23:16:40 +0530

    Add tiktac toe game card

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Portfolio.css";
 import Img from "../../assets/maxresdefault.jpeg";
 import Snake from "../../assets/snake.jpeg";
+import TikTacToe from "../../assets/tik-tac-toe.png";
 
 function Portfolio() {
   return (
@@ -41,6 +42,30 @@ function Portfolio() {
               </a>
               <a
                 href="https://www.github.com"
+                target="_blank"
+                className="btn btn-primary"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <div>
+              <img src={TikTacToe} alt="img" />
+            </div>
+            <h3>Tik Tac Toe Game</h3>
+            <div className="portfolio__item-cta">
+              <a
+                href="https://github.com/akashmore1/Tik-Tak-Toe"
+                target="_blank"
+                className="btn"
+              >
+                Github
+              </a>
+              <a
+                href="https://akashmore1.github.io/Tik-Tak-Toe/"
                 target="_blank"
                 className="btn btn-primary"
               >
