4b4eaae3361567d59e4d1cbbbe58cc3e15a5d210
Akash More <akashkmore1@gmail.com>
2022-10-28 14:11:21 +0530

    Add mouse special animation and effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MouseContext } from "../../features/MouseContext";
 
 import "./Header.css";
 
 const Header = () => {
+  const { cursorChangeHandler } = useContext(MouseContext);
   return (
     <section className="header__section">
       <ul>
         <li>
-          <a href="#home">Home</a>
+          <a
+            href="#home"
+            onMouseEnter={() => cursorChangeHandler("hovered")}
+            onMouseLeave={() => cursorChangeHandler("")}
+          >
+            Home
+          </a>
         </li>
         <li>
-          <a href="#portfolio">Portfolio</a>
+          <a
+            href="#portfolio"
+            onMouseEnter={() => cursorChangeHandler("hovered")}
+            onMouseLeave={() => cursorChangeHandler("")}
+          >
+            Portfolio
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a
+            href="#contact"
+            onMouseEnter={() => cursorChangeHandler("hovered")}
+            onMouseLeave={() => cursorChangeHandler("")}
+          >
+            Contact
+          </a>
         </li>
       </ul>
-      <div className="source__code">
+      <div
+        className="source__code"
+        onMouseEnter={() => cursorChangeHandler("hovered")}
+        onMouseLeave={() => cursorChangeHandler("")}
+      >
         <a
           href="https://github.com/akashmore1/Portfolio-AkashM"
           target="_blank"
